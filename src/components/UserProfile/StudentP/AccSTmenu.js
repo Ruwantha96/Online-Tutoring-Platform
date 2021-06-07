073afe2e1c71703c3b8a8106401e3d28a0b022ab
anjana wimalasena <anjana.wimalasena@gmail.com>
2021-06-07 13:48:41 +0530

    StudentProfile

--- src/components/UserProfile/StudentP/AccSTmenu.js
+++ src/components/UserProfile/StudentP/AccSTmenu.js
@@ -102,7 +102,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   drawerPaper: {
-    color:"white",
+    color:"yellow",
     backgroundColor:grey[900],
     position: 'relative',
     whiteSpace: 'nowrap',
